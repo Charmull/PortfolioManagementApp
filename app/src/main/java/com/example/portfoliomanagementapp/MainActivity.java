605f3174ef103c8860ed42e33d891a9b98542aa3
Charmull <527wlals@gmail.com>
2022-12-09 03:19:44 +0900

    connect firebase and implement upload a picture

--- app/src/main/java/com/example/portfoliomanagementapp/MainActivity.java
+++ app/src/main/java/com/example/portfoliomanagementapp/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.portfoliomanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int GALLERY_CODE = 10;
+    ImageView photo;
+    private FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.Img).setOnClickListener(onClickListener);
+        photo = (ImageView) findViewById(R.id.Img);
+        storage = FirebaseStorage.getInstance();
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.Img:
+                    loadAlbum();
+                    break;
+            }
+        }
+    };
+
+    private void loadAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, GALLERY_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_CODE) {
+            Uri file = data.getData();
+            StorageReference storageRef = storage.getReference();
+            StorageReference riversRef = storageRef.child("photo/1.png");
+            UploadTask uploadTask = riversRef.putFile(file);
+
+            try {
+                InputStream in = getContentResolver().openInputStream(data.getData());
+                Bitmap img = BitmapFactory.decodeStream(in);
+                in.close();
+                photo.setImageBitmap(img);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "사진이 정상적으로 업로드되지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this, "사진이 정상적으로 업로드 되었습니다.", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
