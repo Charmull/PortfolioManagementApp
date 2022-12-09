579102d9ca8375c2d75465c102f9177248021bce
Charmull <527wlals@gmail.com>
2022-12-10 04:52:38 +0900

    complete SpecUpload

---
+++ app/src/main/java/com/example/portfoliomanagementapp/SpecDTO.java
@@ -0,0 +1,8 @@
+package com.example.portfoliomanagementapp;
+
+public class SpecDTO {
+    public Double id;
+    public String title;
+    public String desc;
+    public String category;
+}
--- app/src/main/java/com/example/portfoliomanagementapp/SpecUpload.java
+++ app/src/main/java/com/example/portfoliomanagementapp/SpecUpload.java
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,11 +15,21 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -29,17 +40,35 @@ import java.io.InputStream;
 
 public class SpecUpload extends AppCompatActivity {
 
+    // 사진 관련
     private final int GALLERY_CODE = 10;
-    ImageView photo;
+    ImageView specImg;
     private FirebaseStorage storage;
+    Uri imgFile;
 
+    // 카테고리 관련
+    RadioGroup radioGroup;
+    private String categoryInput;
+
+    // 스펙 제목 관련
+    EditText specTitle;
+
+    // 스펙 설명 관련
+    EditText specDesc;
+
+    // firebase db
+    private FirebaseDatabase database;
+    DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spec_upload);
 
-        photo = (ImageView) findViewById(R.id.specImg);
-        photo.setOnClickListener(new View.OnClickListener() {
+        // 사진 관련
+        specImg = (ImageView) findViewById(R.id.specImg);
+        specImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
@@ -49,14 +78,47 @@ public class SpecUpload extends AppCompatActivity {
                 }
             }
         });
-
         storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        // 카테고리 관련
+        radioGroup = (RadioGroup) findViewById(R.id.selectCategory);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.selectCertificate:
+                        categoryInput = "certificate";
+                        break;
+                    case R.id.selectDegree:
+                        categoryInput = "degree";
+                        break;
+                    case R.id.selectVolunteer:
+                        categoryInput = "volunteer";
+                        break;
+                    case R.id.selectGrade:
+                        categoryInput = "grade";
+                        break;
+                    case R.id.selectAward:
+                        categoryInput = "award";
+                        break;
+                    case R.id.selectEtc:
+                        categoryInput = "etc";
+                        break;
+                }
+            }
+        });
+
+        // 스펙 제목 관련
+        specTitle = (EditText) findViewById(R.id.specTitle);
+
+        // 스펙 설명 관련
+        specDesc = (EditText) findViewById(R.id.specDesc);
     }
 
     private void loadAlbum() {
         Intent intent = new Intent(Intent.ACTION_PICK);
-//        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        intent.setType("image/*");
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, GALLERY_CODE);
     }
 
@@ -64,32 +126,39 @@ public class SpecUpload extends AppCompatActivity {
     protected void onActivityResult(int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GALLERY_CODE) {
-            Uri file = data.getData();
-            StorageReference storageRef = storage.getReference();
-            StorageReference riversRef = storageRef.child("photo/1.png");
-            UploadTask uploadTask = riversRef.putFile(file);
+            imgFile = data.getData();
 
             try {
                 InputStream in = getContentResolver().openInputStream(data.getData());
-                Bitmap specImg = BitmapFactory.decodeStream(in);
+                Bitmap specBit = BitmapFactory.decodeStream(in);
                 in.close();
-                photo.setImageBitmap(specImg);
-//                photo.setImageURI(file);
+                specImg.setImageBitmap(specBit);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-            uploadTask.addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(SpecUpload.this, "사진이 정상적으로 업로드되지 않았습니다.", Toast.LENGTH_LONG).show();
-                }
-            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(SpecUpload.this, "사진이 정상적으로 업로드 되었습니다.", Toast.LENGTH_LONG).show();
-                }
-            });
         }
     }
+
+    public void addSpecBtnHandler(View v) {
+        switch (v.getId()) {
+            case R.id.addSpecBtn:
+                String title = specTitle.getText().toString();
+                String desc = specDesc.getText().toString();
+                double id = Math.random();
+
+                // firebase storage
+                StorageReference storageRef = storage.getReference();
+                StorageReference riversRef = storageRef.child("spec/"+id+"/img.png");
+                UploadTask uploadTask = riversRef.putFile(imgFile);
+
+                // firebase database
+                SpecDTO specDTO = new SpecDTO();
+                specDTO.id = id;
+                specDTO.title = title;
+                specDTO.desc = desc;
+                specDTO.category = categoryInput;
+                dbRef.child("spec").push().setValue(specDTO);
+        }
+    }
+
 }
